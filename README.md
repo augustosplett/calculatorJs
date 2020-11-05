f91fe66a1fc97a6b4b9e2f2b4d8fdb7bcfaeff82
Augusto Madeira Splett <augusto.splett@gmail.com>
2020-11-05 07:03:58 -0300

    implemented the signal changer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # calculatorJs
 This' a learning project, feel free to copy, change, modify etc.
+
+The original idea for this project can be found in: https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Calculator-App.md
--- assets/functions/index.js
+++ assets/functions/index.js
@@ -5,18 +5,23 @@ var display = document.getElementById("display-frame");
 display.value = 0;
 const teclasNumericas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const teclasLimpeza = ['C', 'AC'];
-const teclasOperacao = ['signal-change', 'divide-by', 'times', 'minus', 'plus', 'dot', 'equals'];
+const teclasOperacao = [ 'divide-by', 'times', 'minus', 'plus', 'dot', 'equals'];
+
 
 function tecla(valor) {
-    
+    if(valor == 'signal-change'){
+        display.value = Number(display.value) * -1;  
+    }
     if (teclasNumericas.indexOf(valor) > -1) {
+        if(display.value.length > 7){
+            return
+        };
         if (display.value == valor1 || display.value == 0) {
             display.value = valor;
         } else {
             display.value = display.value + valor;
         }
     }
-
     if (teclasLimpeza.indexOf(valor) > -1) {
         if (valor == 'C') {
             if (operation != 0) {
@@ -28,17 +33,20 @@ function tecla(valor) {
             }
         }
         if (valor == 'AC') {
-            valor1 = 0;
-            valor2 = 0;
-            operation = 0;
             display.value = 0;
+            clearall();
         }
     }
-
     if (teclasOperacao.indexOf(valor) > -1) {
         if (operation != 0) {
             valor2 = display.value;
-            display.value = String(solveOperation());
+            let solve = String(solveOperation());
+            if(solve.length > 7){
+                display.value = 'ERR'
+                clearall();                
+            }else{
+                display.value = solve;
+            }
             valor1 = display.value;
         } else {
             valor1 = display.value;
@@ -52,14 +60,23 @@ function solveOperation() {
     valor1 = Number(valor1);
     valor2 = Number(valor2);
     switch (operation) {
-        case 'divide-by': return valor1 / valor2;
-        case 'times': return valor1 * valor2;
-        case 'minus': return valor1 - valor2;
-        case 'plus': return valor1 + valor2;
+        case 'divide-by':{
+            return valor1 / valor2;
+        } 
+        case 'times':{
+            return valor1 * valor2;
+        } 
+        case 'minus':{
+            return valor1 - valor2;
+        }  
+        case 'plus':{
+            return valor1 + valor2; 
+        } 
     }
 }
 
-
-function displayLength(display) {
-    return display.length;
+function clearall(){
+    valor1 = 0;
+    valor2 = 0;
+    operation = 0;
 }
